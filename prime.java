4c51ffcf8e5a9da08ec8889349a541540b614849
Saumya-Jain-B2K <saumyajain2020@gmail.com>
2025-11-13 22:07:21 +0530

    armstrong and prime number program

---
+++ prime.java
@@ -0,0 +1,18 @@
+public class prime {
+    public static void main(String[] args) {
+        int num = 127;
+        System.out.println(isPrime(num));
+    }
+
+    public static boolean isPrime(int num){
+        if (num <=1){
+            return false;
+        }
+        for (int i = 2; i< Math.sqrt(num); i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
