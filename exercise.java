9273833baa688c47d9a27449d61b53d82edf424e
Saumya-Jain-B2K <saumyajain2020@gmail.com>
2026-04-25 14:39:59 +0530

    normal exercises for java basics

--- exercise.java
+++ exercise.java
@@ -1,5 +1,8 @@
+import java.util.*;
 public class exercise {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //calculate whether a number is divisible by 2,3,4,6 and 12
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a ");        
     }
 }
